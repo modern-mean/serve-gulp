35e42fedf891c5deb76d10799a5a4350ee6b967b
Andrew Throener <athroener@gmail.com>
2016-05-04 10:41:37 -0500

    fix(index): added clean task

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,11 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.watch = exports.nodemon = exports.modules = exports.forever = undefined;
+exports.clean = exports.watch = exports.nodemon = exports.modules = exports.forever = undefined;
+
+var _del = require('del');
+
+var _del2 = _interopRequireDefault(_del);
 
 var _forever = require('./forever');
 
@@ -23,7 +27,16 @@ var watch = _interopRequireWildcard(_watch);
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function clean() {
+  return (0, _del2.default)(['./public/dist']);
+}
+clean.displayName = 'serve:clean';
+gulp.task(clean);
+
 exports.forever = forever;
 exports.modules = modules;
 exports.nodemon = nodemon;
 exports.watch = watch;
+exports.clean = clean;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 'use strict';
 
+import del from 'del';
 import * as forever from './forever';
 import * as modules from './modules';
 import * as nodemon from './nodemon';
 import * as watch from './watch';
 
+function clean() {
+  return del([
+    './public/dist'
+  ]);
+}
+clean.displayName = 'serve:clean';
+gulp.task(clean);
 
-export { forever, modules, nodemon, watch };
+export { forever, modules, nodemon, watch, clean };
